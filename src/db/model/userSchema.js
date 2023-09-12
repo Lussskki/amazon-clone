690ccbfe0d3929930fc498478d648b611421e400
Luka <lussskki@gmail.com>
2023-09-12 16:36:29 +0400

    feat: added method generate token

--- src/db/model/userSchema.js
+++ src/db/model/userSchema.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose"
 import validator from "validator"
 //Hash password
 import bcrypt from 'bcrypt'
-
+//Webtoken
+import jwt from 'jsonwebtoken'
+const secretKey = process.env.KEY
 
 //Schema
 const userSchema = new mongoose.Schema({
@@ -51,11 +53,24 @@ const userSchema = new mongoose.Schema({
 //Hash password
 userSchema.pre('save', async function hash (next){
     if (this.isModified('password')){
-        this.password = await bcrypt.hash(this.password, process.env.SALT)
-        this.cpassword = await bcrypt.hash(this.cpassword, process.env.SALT)
+        this.password = await bcrypt.hash(this.password, 10)
+        this.cpassword = await bcrypt.hash(this.cpassword, 10)
     }
     next()
 })
+//Generate token
+userSchema.methods.generateAuthToken = async function(){
+    try{
+        let token = jwt.sign({_id:this._id},secretKey)
+        this.tokens = this.tokens.concat({token:token})
+        await this.save()
+        
+        return token
+    }catch(err){
+        signale.error(`Generate_token: error- ${err}`)
+    }
+}
+ 
 
 const USER = new mongoose.model('USER', userSchema)
 
