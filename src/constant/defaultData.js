2cdbe84ecea607e8b2155e75c79e5c178e1d043e
Luka <lussskki@gmail.com>
2023-09-12 16:34:33 +0400

    feat: changed snippet

--- src/constant/defaultData.js
+++ src/constant/defaultData.js
@@ -1,16 +1,14 @@
+//schema
 import productsSchema from '../db/model/productsSchema.js'
-
 import productsData from './productsData.js'
+//development
+import signale from 'signale'
 
 
 const defaultData = async ()=>{
     try{
-
         await productsSchema.deleteMany({})
-
-        const storeData = await productsSchema.insertMany(productsData)
-        
-        
+        const storeData = await productsSchema.insertMany(productsData) 
     }catch(err){
         signale.error(`DefaultData: error- ${err}`)
     }
