16bc4268e69de6d290d5266d3f0462765f6e2a71
Luka <lussskki@gmail.com>
2023-09-14 17:37:04 +0400

    feat: v1

--- src/app.js
+++ src/app.js
@@ -1,31 +1,31 @@
 import express from 'express'
 const app = express()
-
 //development
 import signale from 'signale'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 
 
 //db path
 import mongoose from '../src/db/mongo.js'
-
 //default data
 import defaultData from './constant/defaultData.js'
-
 //routes
 import router from './routes/router.js'
-
+//auth
 
 //middleware
 app.use(bodyParser.json())
+app.use(cookieParser(''))
 
 
 //routes
 app.use(router)
 
+
 //function invoke
 defaultData()
-
+ 
 
 
  
