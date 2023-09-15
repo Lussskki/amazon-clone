59b24f255210f8ad8b399db7e4439a812c4c5cf7
Luka <lussskki@gmail.com>
2023-09-15 22:58:44 +0400

    refactored: middleware

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -6,11 +6,11 @@ import USER from '../db/model/userSchema.js'
 const authenticate = async (req, res, next) => {
     try {
       const token = req.cookies['user-auth']
-          //console.log('Received token:', token)
+          console.log('Received token:', token)
       const verifyToken = jwt.verify(token, secretKey)
-          //console.log('Verified token:', verifyToken)
+          console.log('Verified token:', verifyToken)
       const rootUser = await USER.findOne({ _id: verifyToken._id })
-       console.log('Found user:', rootUser)
+          console.log('Found user:', rootUser)
       if (!rootUser) { 
         console.log('User not found for _id:', verifyToken._id) 
         return res.status(404).json({ error: 'User not found' })
@@ -20,9 +20,9 @@ const authenticate = async (req, res, next) => {
       req.userID = rootUser._id
       next();
     } catch (err) {
-      console.error(err)
+       console.error(err)
       res.status(401).json({ error: 'Unauthorized: token provided' }) 
-    }
+    } 
   }
   
   export default authenticate
